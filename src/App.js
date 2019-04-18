c717947abb9de8cee0204e031d6be81aba2f3c96
Justin Teng <nekasrofd@justins-mbp.lan>
2019-04-17 20:12:10 -0700

    win/loss functionality added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ export default class App extends Component {
     super(props);
     this.state = {
       gameStarted: false,
+      playerPlaying: false,
       deckId: "",
       dealerHand: [],
       dealerScore: 0,
+      dealerInitialScore: 0,
       playerHand: [],
       playerScore: 0
     }
   }
 
+  // Fetches deck from Deck of Cards API
   componentDidMount() {
-    fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2`)
+    fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
       .then(res => res.json())
       .then(json => {
         this.setState({
@@ -24,9 +27,10 @@ export default class App extends Component {
       })
   }
 
+  // Return blackjack value of input card value 
   returnValue(value) {
     const cardValues = {
-      ACE: 1,
+      ACE: 11,
       KING: 10,
       QUEEN: 10,
       JACK: 10,
@@ -44,11 +48,7 @@ export default class App extends Component {
     return cardValues[value];
   }
 
-  initialDeal() {
-    
-  }
-
-
+  // When Start Game is pressed
   handleStartGame() {
     fetch(`https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/?count=4`)
       .then(res => res.json())
@@ -59,8 +59,10 @@ export default class App extends Component {
         const value3 = this.returnValue(json.cards[3].value);
         this.setState({
           gameStarted: true,
+          playerPlaying: true,
           dealerHand: [...this.state.dealerHand, json.cards[1], json.cards[3]],
           dealerScore: (this.state.dealerScore += value1 + value3),
+          dealerInitialScore:  value3,
           playerHand: [...this.state.playerHand, json.cards[0], json.cards[2]],
           playerScore: (this.state.playerScore += value0 + value2)
         });
@@ -68,8 +70,9 @@ export default class App extends Component {
       });
   }
 
+  // When End Game is pressed a new deck is drawn
   handleEndGame() {
-    fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2`)
+    fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
     .then(res => res.json())
     .then(json => {
       this.setState({
@@ -78,51 +81,27 @@ export default class App extends Component {
     })
     this.setState({
       gameStarted: false,
+      playerPlaying: false,
       dealerHand: [],
       dealerScore: 0,
-      playerHand: [],
-      playerScore: 0
-    });
-  }
-
-  handleEndHand() {
-    fetch(
-      `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/?count=4`
-    )
-    .then(res => res.json())
-    .then(json => {
-      const value0 = this.returnValue(json.cards[0].value);
-      const value1 = this.returnValue(json.cards[1].value);
-      const value2 = this.returnValue(json.cards[2].value);
-      const value3 = this.returnValue(json.cards[3].value);
-      this.setState({
-        gameStarted: true,
-        dealerHand: [...this.state.dealerHand, json.cards[1], json.cards[3]],
-        dealerScore: (this.state.dealerScore += value1 + value3),
-        playerHand: [...this.state.playerHand, json.cards[0], json.cards[2]],
-        playerScore: (this.state.playerScore += value0 + value2)
-      });
-      this.blackJackChecker();
-    })
-    this.setState({
-      gameStarted: true,
-      dealerHand: [],
-      dealerScore: 0,
+      dealerInitialScore: 0,
       playerHand: [],
       playerScore: 0
     });
   }
 
   blackJackChecker() {
-    if (this.returnValue(this.state.playerHand[0].value) === 10 && this.returnValue(this.state.playerHand[1].value) === 1) {
+    if (this.returnValue(this.state.playerHand[0].value) === 10 && this.returnValue(this.state.playerHand[1].value) === 11) {
       this.setState({
-        playerScore: 21
+        playerScore: 21,
+        playerPlaying: false
       })
       console.log("Blackjack!")
     }
-    if (this.returnValue(this.state.playerHand[1].value) === 10 && this.returnValue(this.state.playerHand[0].value) === 1) {
+    if (this.returnValue(this.state.playerHand[1].value) === 10 && this.returnValue(this.state.playerHand[0].value) === 11) {
       this.setState({
-        playerScore: 21
+        playerScore: 21,
+        playerPlaying: false
       })
       console.log("Blackjack!")
     }
@@ -131,15 +110,28 @@ export default class App extends Component {
   bustChecker() {
     if (this.state.playerScore > 21) {
       console.log("You Busted!")
+      this.setState({
+        playerPlaying: false
+      })
     }
   }
 
+  checkWinner() {
+    if (this.state.dealerScore > 21) {
+      console.log("Dealer busts, you win!")
+    } else if (this.state.playerScore > this.state.dealerScore) {
+      console.log("You Win!")
+    } else if (this.state.playerScore === this.state.dealerScore) {
+      console.log("You Pushed!")
+    } else {
+      console.log("You Lost!")
+    }
+  }
 
-
+  // When player clicks HIT
   handleDrawCard = event => {
     const hand = event.target.value + "Hand";
     const score = event.target.value + "Score";
-    this.bustChecker()
     if (this.state[score] < 21) {
       fetch(
         `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/?count=1`
@@ -150,11 +142,13 @@ export default class App extends Component {
           this.setState({
             [hand]: [...this.state[hand], json.cards[0]],
             [score]: (this.state[score] += newValue)
-          });
-        });
-    }
+          })
+          this.bustChecker()
+        })
+    } 
   };
 
+  // When player clicks STAND
   handleStandEvent = event => {
     if (this.state.dealerScore < 17) {
       fetch(
@@ -164,11 +158,33 @@ export default class App extends Component {
         .then(json => {
           const newValue = this.returnValue(json.cards[0].value);
           this.setState({
+            playerPlaying: false,
             dealerHand: [...this.state.dealerHand, json.cards[0]],
             dealerScore: (this.state.dealerScore += newValue)
           });
+          this.handleStandEvent()
+          this.checkWinner();
         });
+    } else {
+      this.setState({
+        playerPlaying: false
+      })
+      this.checkWinner();
     }
+  }
+
+  // When player clicks DEAL
+  handleDealEvent = event => {
+    this.setState({
+      gameStarted: false,
+      playerPlaying: false,
+      dealerHand: [],
+      dealerScore: 0,
+      dealerInitialScore: 0,
+      playerHand: [],
+      playerScore: 0
+    });
+    this.handleStartGame()
   }
 
   render() {
@@ -186,22 +202,23 @@ export default class App extends Component {
             </button>
           )}
         </div>
-        {this.state.dealerHand.length > 0 ? (
+        {this.state.gameStarted ? (
           <div className="hands-container">
             <Hand
               name="Player"
               cards={this.state.playerHand}
               score={this.state.playerScore}
+              playerPlaying={this.state.playerPlaying}
               handleDrawCard={this.handleDrawCard}
               handleStandEvent={this.handleStandEvent}
+              handleDealEvent={this.handleDealEvent}
             />
             <Hand
               name="Dealer"
               cards={this.state.dealerHand}
-              firstCard={this.state.dealerHand[1]}
               score={this.state.dealerScore}
-              firstScore={this.returnValue(this.state.dealerHand[1].value)}
-              handleDrawCard={this.handleDrawCard}
+              playerPlaying={this.state.playerPlaying}
+              dealerInitialScore={this.state.dealerInitialScore}
             />
           </div>
         ) : (
--- src/components/Hand.js
+++ src/components/Hand.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Card from "./Card.js";
 
 export default class Hand extends Component {
-  
+
   render() {
     let cards;
     if (this.props.cards.length) {
@@ -20,28 +20,48 @@ export default class Hand extends Component {
         {this.props.name === "Dealer" ? (
           <div>
             <h3>{this.props.name}</h3>
-            <h5>{this.props.score}</h5>
-            <div className="cards-container">{cards}</div>
+            {this.props.playerPlaying ? (
+                <h5>{this.props.dealerInitialScore}</h5>
+            ) : (
+                <h5>{this.props.score}</h5>
+            )}
+            {this.props.playerPlaying ? (
+                <div className="cards-container">{cards[1]}</div>
+            ) : (
+                <div className="cards-container">{cards}</div>
+            )}
+            
           </div>
           ) : (
             <div>
             <h3>{this.props.name}</h3>
             <h5>{this.props.score}</h5>
-            <button
-              className="sm"
-              onClick={event => this.props.handleDrawCard(event)}
-              value={this.props.name.toLowerCase()}
-            >
-              Hit
-            </button>
-            <button
-              className="sm"
-              onClick={event => this.props.handleStandEvent(event)}
-              value={this.props.name.toLowerCase()}
-            >
-              Stand
-            </button>
-
+            {this.props.playerPlaying ? (
+                <div className="playingButtons">
+                    <button
+                    className="sm"
+                    onClick={event => this.props.handleDrawCard(event)}
+                    value={this.props.name.toLowerCase()}
+                    >
+                    Hit
+                    </button>
+                    <button
+                    className="sm"
+                    onClick={event => this.props.handleStandEvent(event)}
+                    value={this.props.name.toLowerCase()}
+                    >
+                    Stand
+                    </button>
+                </div>
+            ) : (
+                <button
+                className="sm"
+                onClick={event => this.props.handleDealEvent(event)}
+                value={this.props.name.toLowerCase()}
+                >
+                Deal
+                </button>
+            )}
             <div className="cards-container">{cards}</div>
             </div>
           )}
