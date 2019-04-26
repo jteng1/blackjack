78c440ee1353cb61a5f839a6db1506a25e1ce2dd
Justin Teng <nekasrofd@Justins-MacBook-Pro.local>
2019-04-26 16:05:05 -0700

    modified .gitignore file to remove ignoring build/

---
+++ build/precache-manifest.9cba7b8578b2d181a4b0babfb1b35f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b5d81722050a63729b0dda37aec3354",
+    "url": "/static/media/blankCard.3b5d8172.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c500e380ca1507003665",
+    "url": "/static/js/main.fd34594f.chunk.js"
+  },
+  {
+    "revision": "6220c4b8b947b344436e",
+    "url": "/static/js/2.fcf2d699.chunk.js"
+  },
+  {
+    "revision": "c500e380ca1507003665",
+    "url": "/static/css/main.9bcb5559.chunk.css"
+  },
+  {
+    "revision": "fb1d3b22aec21489e1fa9c3a5aea582c",
+    "url": "/index.html"
+  }
+];
