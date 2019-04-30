877bd07d379ca2dd263ba6df5b9b75239b5132a2
Justin Teng <nekasrofd@Justins-MacBook-Pro.local>
2019-04-29 17:40:29 -0700

    Updates

---
+++ precache-manifest.55dfaf42dd77e30697824923877b0215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a5af5d74623adc3c5cb46cfcc9de11",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "7a57181a51d3a411306f",
+    "url": "/blackjack/static/css/main.38061498.chunk.css"
+  },
+  {
+    "revision": "6220c4b8b947b344436e",
+    "url": "/blackjack/static/js/2.fcf2d699.chunk.js"
+  },
+  {
+    "revision": "7a57181a51d3a411306f",
+    "url": "/blackjack/static/js/main.b6980ece.chunk.js"
+  },
+  {
+    "revision": "499199909dcd85e1cabf",
+    "url": "/blackjack/static/js/runtime~main.73e4a0a6.js"
+  },
+  {
+    "revision": "3b5d81722050a63729b0dda37aec3354",
+    "url": "/blackjack/static/media/blankCard.3b5d8172.jpg"
+  }
+]);
