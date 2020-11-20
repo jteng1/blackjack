b89bc710db9e6216a7d4524d3333b84415b34afa
Justin Teng <nekasrofd@justins-mbp.lan>
2020-11-19 22:53:07 -0800

    Updates

---
+++ precache-manifest.f7e7ff245afe82cd497b12acab353fd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f765d2bef7e7d5d75392d0fcdaab275",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "e8c48ebfbcf58877dc1f",
+    "url": "/blackjack/static/css/main.16aa2e6c.chunk.css"
+  },
+  {
+    "revision": "d257413e13baaa854fc9",
+    "url": "/blackjack/static/js/2.d6eb7a65.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/blackjack/static/js/2.d6eb7a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c48ebfbcf58877dc1f",
+    "url": "/blackjack/static/js/main.a9b5c083.chunk.js"
+  },
+  {
+    "revision": "68734dc473247443cdbc",
+    "url": "/blackjack/static/js/runtime-main.852dc962.js"
+  },
+  {
+    "revision": "3b5d81722050a63729b0dda37aec3354",
+    "url": "/blackjack/static/media/blankCard.3b5d8172.jpg"
+  }
+]);
